cc6ea43b7539f71ccf298efae6b7c775cde50eec
Henrique Rosa <samuel_kuik@hotmail.com>
2022-04-27 16:20:28 -0300

    Exercicio quase pronto

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,5 @@
+let buttonCont = document.getElementById("buttons")
+let createBTN = document.createElement('button')
+buttonCont.appendChild(createBTN)
+createBTN.innerText = "Limpar"
+createBTN.id = "btn"
