2f556faf36bcca3766cc99f5bc71c6aac875b0aa
Henrique Rosa <samuel_kuik@hotmail.com>
2022-04-27 20:01:38 -0300

    Feito alterções usado o bootStrap

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -2,4 +2,6 @@ let buttonCont = document.getElementById("buttons")
 let createBTN = document.createElement('button')
 buttonCont.appendChild(createBTN)
 createBTN.innerText = "Limpar"
-createBTN.id = "btn"
+createBTN.id = "btnClear"
+createBTN.setAttribute("type", "reset")
+createBTN.className = "btn btn-outline-danger"
